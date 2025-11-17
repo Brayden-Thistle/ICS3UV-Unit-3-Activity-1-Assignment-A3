141de9734edfe5527712cc3896753c598e6a7593
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-11-17 15:00:54 +0000

    fixed more typos

--- main.ts
+++ main.ts
@@ -6,42 +6,42 @@
  */
 
 //equation 1
-console.log("0 X 9 = ", (0 * 9))
+console.log("0 X 9 = ", (0 * 9));
 
 //equation 2
-console.log("1 X 9 = ", (1 * 9))
+console.log("1 X 9 = ", (1 * 9));
 
 //equation 3 
-console.log("2 X 9 = ", (2 * 9))
+console.log("2 X 9 = ", (2 * 9));
 
 //equation 4
-console.log("3 X 9 = ", (3 * 9))
+console.log("3 X 9 = ", (3 * 9));
 
 //equation 5
-console.log("4 X 9 =", (4 * 9))
+console.log("4 X 9 =", (4 * 9));
 
 //equation 6
-console.log("5 X 9 = ", (5 * 9))
+console.log("5 X 9 = ", (5 * 9));
 
 //equation 7
-console.log("6 X 9 = ", (6 * 9))
+console.log("6 X 9 = ", (6 * 9));
 
 //equation 8
-console.log("7 X 9 = ", (7 * 9))
+console.log("7 X 9 = ", (7 * 9));
 
 //equation 9
-console.log("8 X 9 = ", (8 * 9))
+console.log("8 X 9 = ", (8 * 9));
 
 //equation 10
-console.log("9 X 9 = ", (9 * 9))
+console.log("9 X 9 = ", (9 * 9));
 
 //equation 11
-console.log("10 X 9 = ", (10 * 9))
+console.log("10 X 9 = ", (10 * 9));
 
 //equation 12
-console.log("11 X 9 = ", (11 * 9))
+console.log("11 X 9 = ", (11 * 9));
 
 //equation 13
-console.log("12 X 9 = ", (12 * 9))
+console.log("12 X 9 = ", (12 * 9));
 
-console.log("\nDone.")
+console.log("\nDone.");
