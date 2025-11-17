583494e66c7f3b9264d1de3062f370032052715e
Brayden Thistle <bthistle@huronperthcatholic.ca>
2025-11-17 14:25:25 +0000

    added a TS and GO program that displays multiples of 9, up to 12

---
+++ main.go
@@ -0,0 +1,54 @@
+/*
+* @author Brayden Thistle
+* @version 1.0.0
+* @date 2025-11-14
+* @fileoverfiew this program displays the area of a circle
+*/
+
+package main
+
+import "fmt"
+
+func main () {
+
+//equation 1
+fmt.Println("0 X 9 = ", (0 * 9))
+
+//equation 2
+fmt.Println("1 X 9 = ", (1 * 9))
+
+//equation 3 
+fmt.Println("2 X 9 = ", (2 * 9))
+
+//equation 4
+fmt.Println("3 X 9 = ", (3 * 9))
+
+//equation 5
+fmt.Println("4 X 9 =", (4 * 9))
+
+//equation 6
+fmt.Println("5 X 9 = ", (5 * 9))
+
+//equation 7
+fmt.Println("6 X 9 = ", (6 * 9))
+
+//equation 8
+fmt.Println("7 X 9 = ", (7 * 9))
+
+//equation 9
+fmt.Println("8 X 9 = ", (8 * 9))
+
+//equation 10
+fmt.Println("9 X 9 = ", (9 * 9))
+
+//equation 11
+fmt.Println("10 X 9 = ", (10 * 9))
+
+//equation 12
+fmt.Println("11 X 9 = ", (11 * 9))
+
+//equation 13
+fmt.Println("12 X 9 = ", (12 * 9))
+
+fmt.Println("\nDone.")
+}
